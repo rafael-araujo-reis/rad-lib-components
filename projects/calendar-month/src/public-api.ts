05e5ed6c21d3da8837e2f910515c9fef760b5156
Rafael Reis <rafa.araujoreis@gmail.com>
2022-06-04 11:46:21 -0300

    change local component

--- projects/calendar-month/src/public-api.ts
+++ projects/calendar-month/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export * from './lib/services/calendar-month.service';
 export * from './lib/calendar-month.component';
-export * from './lib/modules/calendar-month.module';
+export * from './lib/calendar-month.module';
--- projects/mat-sign-in/src/public-api.ts
+++ projects/mat-sign-in/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export * from './lib/services/mat-sign-in.service';
 export * from './lib/mat-sign-in.component';
-export * from './lib/modules/mat-sign-in.module';
+export * from './lib/mat-sign-in.module';
